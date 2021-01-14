93d9478de4fcaee380c258b93c98c7283a993f2d
Ben Milligan <32855321+Nagillimi@users.noreply.github.com>
2021-01-13 19:21:32 -0500

    Delete Algorithms/Test Data 250Hz directory

--- Algorithms/Test Data 250Hz/ShperToRec.m
+++
@@ -1,7 +0,0 @@
-% Rectangular to spherical coord function
-% Returns a 3x3 Matrix of rectangular coords from spherical coords
-function ShperToRec = ShperToRec(a,i)
-    ShperToRec = [cos(a)*cos(i), -sin(a), -cos(a)*sin(i);
-                  sin(a)*cos(i), cos(a), -sin(a)*sin(i);
-                     sin(i),        0,        cos(i)  ];
-end
