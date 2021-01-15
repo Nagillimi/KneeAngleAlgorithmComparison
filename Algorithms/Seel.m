338dac5ddce2045ed0f801799faf364d3b1f8457
Ben Milligan <32855321+Nagillimi@users.noreply.github.com>
2021-01-15 16:43:23 -0500

    Update Seel.m

--- Algorithms/Seel.m
+++ Algorithms/Seel.m
@@ -1,33 +1,34 @@
 function [acceldata,accel_angle,gyro_angle,knee_angle_Seel] = Seel(gx_1,gy_1,gz_1,...
     ax_1,ay_1,az_1,gx_2,gy_2,gz_2,ax_2,ay_2,az_2,stepper_knee_angle,gait_stage)
     % As defined in (Seel, 2014), can range from [0,1]. 
-    % The sensor fusion constant
-    lambda = 0.001; 
+    % The sensor fusion constant, he did 0.01
+    lambda = 0.01; 
     
     % Vector maintains a non-zero product with j1 and j2 (can't be parallel
     % to either j1 or j2!). Init as [1,0,0]' as mentioned in (Seel, 2014).
-    c = [1,0,0]';
+    c = [0,1,0]';
     
     % Collect individual datasets
     gyrodata = [gx_1,gy_1,gz_1,gx_2,gy_2,gz_2]';
     acceldata_ = [ax_1,ay_1,az_1,ax_2,ay_2,az_2]';
     
     % Correct acceldata (from calibration error)
-    acceldata = CorrectAccelBias(acceldata_,stepper_knee_angle,gait_stage);
+%     acceldata = CorrectAccelBias(acceldata_,stepper_knee_angle,gait_stage);
+    acceldata = acceldata_;
     
     % Length of dataset
     N = length(gyrodata(1,:));
     
     % Obtaining j vectors, following Seel's 2014 study
-    [j1,j2,~] = localJ(gyrodata,true);
+    [j1,j2,~] = localJ(gyrodata,0);
     
     % Obtaining o-vectors, following Seel's 2012/2014 study 
-    [~,~,gyro_dotdata,~] = localO(acceldata,gyrodata,true);
+    [o1,o2,gyro_dotdata,~] = localO(acceldata,gyrodata,0);
     
     %====================================================================
-    % Overwrite o-vectors for testing, measured from ImageJ.
-    o1 = [0.644,-0.715,0]';
-    o2 = [0.063,0.998,0]';
+    % Overwrite o-vectors for testing, approx using ImageJ.
+%     o1 = [0.644,-0.715,0]';
+%     o2 = [0.063,0.998,0]';
     %====================================================================
     
     % Shift o-vectors to be closer to sensors. "The final step in of an 
@@ -35,17 +36,19 @@ function [acceldata,accel_angle,gyro_angle,knee_angle_Seel] = Seel(gx_1,gy_1,gz_
     o1 = o1 - j1*(dot(o1,j1) + dot(o2,j2))/2;
     o2 = o2 - j2*(dot(o1,j1) + dot(o2,j2))/2;
     
+    %---------------------------GYRO-------------------------------------
     % Gyro-derived angle
-    gyro_angle_ = zeros(N,1); gyro_angle = zeros(N,1);
+    gyro_angle_ = zeros(N,1); 
+    % Time Vector
+    t = (1/100).*(1:1:N)'; 
     for i = 1:N
         % Integrate
         gyro_angle_(i) = dot(gyrodata(1:3,i),j1) - dot(gyrodata(4:6,i),j2);
     end
-    % Populate integral
-    for i = 2:N-1
-        gyro_angle(i-1) = mean(gyro_angle_(i-1:i+1)).*2;
-    end
+    % Populate integral with cumtrapz()
+    gyro_angle = cumtrapz(t, gyro_angle_);
         
+    %---------------------------ACCEL------------------------------------
     % Define joint axes for accel data
     x1 = cross(j1,c); y1 = cross(j1,x1);
     x2 = cross(j2,c); y2 = cross(j2,x2);
@@ -66,46 +69,83 @@ function [acceldata,accel_angle,gyro_angle,knee_angle_Seel] = Seel(gx_1,gy_1,gz_
         a2_lambda(1:3,i) = acceldata(4:6,i) - rot2;
         
         % Constructing 2d accel vectors in joint frame
-        u(1:2,i) = [dot(a1_lambda(:,i),x1),dot(a1_lambda(:,i),y1)]';
-        v(1:2,i) = [dot(a2_lambda(:,i),x2),dot(a2_lambda(:,i),y2)]';
+        u(1:2,i) = [(dot(a1_lambda(:,i),x1)),(dot(a1_lambda(:,i),y1))]';
+        v(1:2,i) = [(dot(a2_lambda(:,i),x2)),(dot(a2_lambda(:,i),y2))]';
     end
     
     % Accel-derived angle
+    alpha = zeros(N,1); beta = zeros(N,1);
     for i = 1:N
         % Accel-derived angle
         
-        % Complex plane solution
-%         x = dot(u(:,i), [1, 1i]');
-%         y = dot(v(:,i), [1, 1i]');
-%         accel_angle(i) = angle(x*y');
+        % Double Angle solution. [atan(y/x)]
+        alpha(i) = abs(atand(u(2,i)/u(1,i))); beta(i) = abs(atand(v(2,i)/v(1,i)));
         
-        % Other vectorized solution
-%         accel_angle(i) = atan2d(u(:,i) * (v([2,1],i)'.*[1 -1]).',u(:,i) * v(:,i).');
+        % Various angle conditions
         
-        % Normalized vector dot Product
-%         uN = u(:,i)./norm(u(:,i)); vN = v(:,i)./norm(v(:,i));
-%         accel_angle(i) = acosd(dot(uN,vN))./5;
+%         % Opposite ends of x-coord OR y-coord
+%         if (u(1,i) > 0 && v(1,i) < 0) || (u(2,i) > 0 && v(2,i) < 0) || ...
+%            (v(1,i) > 0 && u(1,i) < 0) || (v(2,i) > 0 && u(2,i) < 0)
+%             accel_angle(i) = beta(i) + alpha(i);            
+% 
+%         % Same grid of x-coord OR y-coord
+%         elseif (u(1,i) > 0 && v(1,i) > 0 && u(2,i) > 0 && v(2,i) > 0) || ...
+%                (u(1,i) < 0 && v(1,i) < 0 && u(2,i) < 0 && v(2,i) < 0) || ...
+%                (u(1,i) > 0 && v(1,i) > 0 && u(2,i) < 0 && v(2,i) < 0) || ...
+%                (u(1,i) < 0 && v(1,i) < 0 && u(2,i) > 0 && v(2,i) > 0)
+%             accel_angle(i) = (beta(i) - alpha(i));
+%             
+%         % Diagonally opposite grid of x-coord OR y-coord
+%         elseif (u(1,i) > 0 && v(1,i) < 0 && u(2,i) > 0 && v(2,i) < 0) || ...
+%                (u(1,i) < 0 && v(1,i) > 0 && u(2,i) < 0 && v(2,i) > 0)
+%             accel_angle(i) = (beta(i) - alpha(i));
+%         end
         
-        % Definition of Dot Product
-%         accel_angle(i) = acosd((dot(v(1:2,i),u(1:2,i)))...
-%             /(norm(v(1:2,i))*norm(u(1:2,i))));
+        accel_angle(i) = (beta(i) - alpha(i));
+%         % u on RS, v on LS
+%         if (u(1,i)>0) && (u(2,i)>0) && (v(1,i)<0) && (v(2,i)>0)
+%             accel_angle(i) = alpha(i) + beta(i);
+%         elseif (u(1,i)>0) && (u(2,i)>0) && (v(1,i)<0) && (v(2,i)<0)
+%             accel_angle(i) = alpha(i) + beta(i);
+%         elseif (u(1,i)>0) && (u(2,i)<0) && (v(1,i)<0) && (v(2,i)>0)
+%             accel_angle(i) = alpha(i) + beta(i);
+%         elseif (u(1,i)>0) && (u(2,i)<0) && (v(1,i)<0) && (v(2,i)<0)
+%             accel_angle(i) = alpha(i) + beta(i);
+%             
+%         % u on LS, v on RS
+%         elseif (v(1,i)>0) && (v(2,i)>0) && (u(1,i)<0) && (u(2,i)>0)
+%             accel_angle(i) = alpha(i) + beta(i);
+%         elseif (v(1,i)>0) && (v(2,i)>0) && (u(1,i)<0) && (u(2,i)<0)
+%             accel_angle(i) = alpha(i) + beta(i);
+%         elseif (v(1,i)>0) && (v(2,i)<0) && (u(1,i)<0) && (u(2,i)>0)
+%             accel_angle(i) = alpha(i) + beta(i);
+%         elseif (v(1,i)>0) && (v(2,i)<0) && (u(1,i)<0) && (u(2,i)<0)
+%             accel_angle(i) = alpha(i) + beta(i);
+%         
+%         % u on RS, v on RS
+%         elseif (u(1,i)>0) && (u(2,i)>0) && (v(1,i)>0) && (v(2,i)>0)
+%             accel_angle(i) = beta(i) - alpha(i);
+%         elseif (u(1,i)>0) && (u(2,i)<0) && (v(1,i)>0) && (v(2,i)>0)
+%             accel_angle(i) = beta(i) - alpha(i);
+%         elseif (u(1,i)>0) && (u(2,i)<0) && (v(1,i)>0) && (v(2,i)<0)
+%             accel_angle(i) = beta(i) - alpha(i);
+%         elseif (u(1,i)>0) && (u(2,i)>0) && (v(1,i)>0) && (v(2,i)<0)
+%             accel_angle(i) = beta(i) - alpha(i);
+%         
+%         % u on LS, v on LS
+%         elseif (v(1,i)>0) && (v(2,i)>0) && (u(1,i)>0) && (u(2,i)>0)
+%             accel_angle(i) = beta(i) - alpha(i);
+%         elseif (v(1,i)>0) && (v(2,i)<0) && (u(1,i)>0) && (u(2,i)>0)
+%             accel_angle(i) = beta(i) - alpha(i);
+%         elseif (v(1,i)>0) && (v(2,i)<0) && (u(1,i)>0) && (u(2,i)<0)
+%             accel_angle(i) = beta(i) - alpha(i);
+%         elseif (v(1,i)>0) && (v(2,i)>0) && (u(1,i)>0) && (u(2,i)<0)
+%             accel_angle(i) = beta(i) - alpha(i);
+%         end
         
-        % Definition of Cross Product
-%         accel_angle(i) = asind( (u(1,i)*v(2,i) - u(2,i)*v(1,i))...
-%             ./(norm(u(:,i))*norm(v(:,i))) );
-        
-%         accel_angle(i) = acosd(min(1,max(-1, u(:,i).'*v(:,i) / norm(u(:,i)) / norm(v(:,i)) )));
-        
-
-%         CosTheta = max(min(dot(u(:,i),v(:,i))/(norm(u(:,i))*norm(v(:,i))),1),-1);
-%         accel_angle(i) = real(acosd(CosTheta));
-        
-
-        % https://www.mathworks.com/matlabcentral/answers/180131-how-can-i-find-the-angle-between-two-vectors-including-directional-information
-        accel_angle(i) = atan2d(u(1,i)*v(2,i) - u(2,i)*v(1,i),...
-                                u(1,i)*v(1,i) + u(2,i)*v(2,i));
     end
     
+    %---------------------------COMBINE----------------------------------    
     % Sensor fusion (Seel, 2014)
     knee_angle_Seel = zeros(N,1);
     for i = 2:N
@@ -115,8 +155,47 @@ function [acceldata,accel_angle,gyro_angle,knee_angle_Seel] = Seel(gx_1,gy_1,gz_
     end
     
     % Low Pass filter
-    [Bl,Al] = butter(2,1.5/100,'low');
+    [Bl,Al] = butter(2,2/100,'low');
     knee_angle_Seel = filtfilt(Bl,Al,knee_angle_Seel);
     gyro_angle = filtfilt(Bl,Al,gyro_angle);
     accel_angle = filtfilt(Bl,Al,accel_angle);
+%     u(1,:) = filtfilt(Bl,Al,u(1,:));
+%     v(1,:) = filtfilt(Bl,Al,v(1,:));
+%     u(2,:) = filtfilt(Bl,Al,u(2,:));
+%     v(2,:) = filtfilt(Bl,Al,v(2,:));
+%     alpha = filtfilt(Bl,Al,alpha);
+%     beta = filtfilt(Bl,Al,beta);
 end
+
+
+% figure(2)
+% plot(u(1,:))
+% hold on
+% plot(v(1,:))
+% title('X-Coordinate JCS')
+% 
+% figure(3)
+% plot(u(2,:))
+% hold on
+% plot(v(2,:))
+% title('Y-Coordinate JCS')
+% 
+% figure(4)
+% subplot(2,1,1)
+% plot(v(1,:)-u(1,:))
+% title('Difference between X-Coordinates')
+% subplot(2,1,2)
+% plot(v(2,:)-u(2,:))
+% title('Difference between Y-Coordinates')
+% 
+% figure(5)
+% plot(accel_angle(1:5000))
+% hold on
+% plot(gyro_angle(1:5000))
+% plot(knee_angle_Seel(1:5000))
+% 
+% figure(6)
+% plot(alpha)
+% hold on
+% plot(beta)
+% legend('alpha','beta')
