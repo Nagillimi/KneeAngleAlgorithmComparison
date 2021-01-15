864b9d2374c4b3611fbc865fc56b02d28910cbc0
Ben Milligan <32855321+Nagillimi@users.noreply.github.com>
2021-01-15 17:07:17 -0500

    Update Allseits.m
    Update

--- Algorithms/Allseits.m
+++ Algorithms/Allseits.m
@@ -1,12 +1,28 @@
 function knee_angle_Allseits = Allseits(gx_1,gy_1,gz_1,gx_2,gy_2,gz_2)
-    
     % Collect individual datasets
     gyrodata = [gx_1,gy_1,gz_1,gx_2,gy_2,gz_2]';
     
-    % Obtaining j-vectors, following Seel's 2012/2014 study
-    [j1,j2] = localJ(gyrodata,true);
-    J = [j1,j2];
-
+    % Length of dataset
+    N = length(gx_1);
+    
+    % Obtaining j vectors, following Seel's 2014 study
+    [j1,j2,~] = localJ(gyrodata,0);
+    
+    %---------------------------GYRO-------------------------------------
+    % Gyro-derived angle
+    gyro_angle_ = zeros(N,1); 
+    % Time Vector
+    t = (1/100).*(1:1:N)'; 
+    for i = 1:N
+        % Integrate
+        gyro_angle_(i) = dot(gyrodata(1:3,i),j1) - dot(gyrodata(4:6,i),j2);
+    end
+    % Populate integral with cumtrapz()
+    gyro_angle = cumtrapz(t, gyro_angle_);
+    %--------------------------------------------------------------------
+    
+    % From old revision:
+    
     % Converting g's to global CS. Follow a Cardan Rotation Sequence = XZ. 
     % Puts y parallel to joint vector
     for i = 1:2
