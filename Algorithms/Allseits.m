9e7e3ea719ad13d4414f6f0b27caadae836e962b
Ben Milligan <32855321+Nagillimi@users.noreply.github.com>
2021-01-16 21:07:12 -0500

    Update Allseits.m

--- Algorithms/Allseits.m
+++ Algorithms/Allseits.m
@@ -1,4 +1,4 @@
-function knee_angle_Allseits = Allseits(gx_1,gy_1,gz_1,gx_2,gy_2,gz_2)
+function knee_angle_Allseits = Allseits(f,gx_1,gy_1,gz_1,gx_2,gy_2,gz_2)
     % Collect individual datasets
     gyrodata = [gx_1,gy_1,gz_1,gx_2,gy_2,gz_2]';
     
@@ -8,41 +8,63 @@ function knee_angle_Allseits = Allseits(gx_1,gy_1,gz_1,gx_2,gy_2,gz_2)
     % Obtaining j vectors, following Seel's 2014 study
     [j1,j2,~] = localJ(gyrodata,0);
     
-    %---------------------------GYRO-------------------------------------
-    % Gyro-derived angle
-    gyro_angle_ = zeros(N,1); 
-    % Time Vector
-    t = (1/100).*(1:1:N)'; 
+    % Convert gyros to JCS (using j's).
+    % c can't be parallel to either j-vector
+    c = [0,1,0]'; 
+    
+    % Define joint axes orthogonal to joint vector
+    x1 = cross(j1,c); y1 = cross(j1,x1);
+    x2 = cross(j2,c); y2 = cross(j2,x2);
+    
+    % Init
+    delta_g = zeros(3,N); g1_JCS = zeros(3,N); g2_JCS = zeros(3,N);
+    
+    % Populate gyro data to JCS components
     for i = 1:N
-        % Integrate
-        gyro_angle_(i) = dot(gyrodata(1:3,i),j1) - dot(gyrodata(4:6,i),j2);
-    end
-    % Populate integral with cumtrapz()
-    gyro_angle = cumtrapz(t, gyro_angle_);
-    %--------------------------------------------------------------------
-    
-    % From old revision:
-    
-    % Converting g's to global CS. Follow a Cardan Rotation Sequence = XZ. 
-    % Puts y parallel to joint vector
-    for i = 1:2
-        % Test the signs of these rotations...
-        a = atan(J(3,i)); b = atan(J(1,i)/sqrt(J(3,i)^2+J(2,i)^2));
-        R = [1, 0, 0;
-            0, cos(a), -sin(a);
-            0, sin(a), cos(a)] * [cos(b), 0, sin(b);
-                                  0, 1, 0;
-                                  -sin(b), 0, cos(b)];
-        % (#sensor,rows,cols)
-        disp('R = ')
-        disp(R)
-%         G(i,3,:) = R*[gx_1,gy_1,gz_1]';
+        g1_JCS(1:3,i) = [dot(gyrodata(1:3,i),x1),dot(gyrodata(1:3,i),y1),dot(gyrodata(1:3,i),j1)]';
+        g2_JCS(1:3,i) = [dot(gyrodata(4:6,i),x2),dot(gyrodata(4:6,i),y2),dot(gyrodata(4:6,i),j2)]';
+
+        delta_g(1:3,i) = [g1_JCS(1,i)-g2_JCS(1,i),g1_JCS(2,i)-g2_JCS(2,i),g1_JCS(3,i)-g2_JCS(3,i)]';
     end
     
+    % LP filter
+    [Bl,Al] = butter(2,1.5/100,'low');
+    delta_g(3,:) = filtfilt(Bl,Al,delta_g(3,:));
+    g2_JCS(3,:) = filtfilt(Bl,Al,g2_JCS(3,:));
     
+    % ZKA Algorithm to find the ZKA indices from shank gyro data
+    zero_index = ZKA(g2_JCS(3,:),8);
+        
+    % Integrate delta of FE data (Z) to get knee angle
+    t = (1/f).*(1:1:N)'; 
+    knee_angle_Allseits = cumtrapz(t, delta_g(3,:));
     
+    % Apply the ZKA indices to correct drift
+    for i = 1:8
+        zero_bias = knee_angle_Allseits(zero_index(i)) - 0; % @ Zero angle
+        knee_angle_Allseits(zero_index(i):end) = knee_angle_Allseits(zero_index(i):end) - zero_bias;
+        
+%         hs_bias = knee_angle_Allseits(hs_index(i)) - 4; % @ 4deg angle
+%         knee_angle_Allseits(hs_index(i):zero_index(i+1)) = knee_angle_Allseits(hs_index(i):zero_index(i+1)) + hs_bias;
+    end
     
-    % Computing Knee angle etc
-    knee_angle_Allseits = 1;
-    error_Allseits = 1;
+    % Debugging Plots
+%     figure(1)
+%     t = zeros(1,N);
+%     plot(g2_JCS(1,1:end))
+%     hold on
+%     plot(g2_JCS(2,1:end))
+%     plot(g2_JCS(3,1:end))
+%     plot(t(1:1000),'k:')
+%     plot(stepper_knee_angle(1:end))
+%     legend('x','y','z')
+%     hold off
+%     
+%     figure(2)
+%     plot(stepper_knee_angle)
+%     hold on
+%     plot(knee_angle_Allseits)
+%     plot(t,'k:') 
+%     hold off
+    
 end
